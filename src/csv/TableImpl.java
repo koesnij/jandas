ab0ffb514f915859a1fef39974a68c1ca65ae256
문진석 <m_jinseok@naver.com>
2021-06-12 20:18:40 +0900

    feat: Print dtype in toString()

--- src/csv/TableImpl.java
+++ src/csv/TableImpl.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 
 class TableImpl implements Table {
     private final boolean hasHeader;
-    private class Pair<X, Y> {
+    private final static class Pair<X, Y> {
         private X x;
         private Y y;
 
@@ -39,9 +39,10 @@ class TableImpl implements Table {
 
     @Override
     public String toString() {
+        int[] dtypes = {0, 0, 0}; // for dtypes count
         String result =
                   "<csv.Table@" + Integer.toHexString(hashCode()) + ">\n"
-                + "RangeIndex: " + columns.get(0).count() + ", 0 to " + (columns.get(0).count() - 1) + "\n"
+                + "RangeIndex: " + columns.get(0).count() + " entries, 0 to " + (columns.get(0).count() - 1) + "\n"
                 + "Data columns (total " + columns.size() + " columns): \n";
 
         // Metadata table
@@ -69,9 +70,18 @@ class TableImpl implements Table {
             line.add(currentColumn.count() - currentColumn.getNullCount() + " non-null");
 
             // Set dtype
-            if (((ColumnImpl) currentColumn).isIntegerColumn()) line.add("int");
-            else if (currentColumn.isNumericColumn()) line.add("double");
-            else line.add("String");
+            if (((ColumnImpl) currentColumn).isIntegerColumn()) {
+                ++dtypes[1];
+                line.add("int");
+            }
+            else if (currentColumn.isNumericColumn()) {
+                ++dtypes[0];
+                line.add("double");
+            }
+            else {
+                ++dtypes[2];
+                line.add("String");
+            }
 
             // Set width
             width.set(0, Math.max(width.get(0), line.get(0).length()));
@@ -90,6 +100,7 @@ class TableImpl implements Table {
                     + String.format("%" + width.get(2) + "s", row.get(2)) + " |"
                     + String.format("%-" + width.get(3) + "s", row.get(3)) + "\n";
         }
+        result += "dtypes: double(" + dtypes[0] + "), int(" + dtypes[1] + "), String(" + dtypes[2] + ")";
 
         return result;
     }
@@ -431,7 +442,7 @@ class TableImpl implements Table {
         boolean isModified = false;
 
         for (int i = 0; i < getColumnCount(); ++i) {
-            if (columns.get(i).fillNullWithMean()) {
+            if (getColumn(i).fillNullWithMean()) {
                 isModified = true;
             }
         }
